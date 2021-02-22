f4d0b1e06d8a385305db440f9b4fef1d44dd427d
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-02-23 10:43:04 +1100

    Add tests for readers and writers

--- napari_compressed_labels_io/_tests/test_reader.py
+++ napari_compressed_labels_io/_tests/test_reader.py
@@ -1,30 +1,50 @@
 import numpy as np
-from napari_compressed_labels_io import napari_get_reader
+import os
+
+from numpy.testing._private.utils import assert_array_almost_equal
+from napari_compressed_labels_io import get_zarr_labels, get_label_image_stack
+from napari_compressed_labels_io._writer import labels_to_zarr, write_label_image_pairs
 
 
-# tmp_path is a pytest fixture
-def test_reader(tmp_path):
-    """An example of how you might test your plugin."""
+def test_get_zarr_labels_successful(tmp_path):
+    out_pth = os.path.join(tmp_path, 'out_labels.zarr')
+    data = np.random.randint(4, size=(3, 100, 100))
+    meta = {
+        'name': 'test_labels'
+    }
+    labels_to_zarr(out_pth, data, meta)
 
-    # write some fake data using your supported file format
-    my_test_file = str(tmp_path / "myfile.npy")
-    original_data = np.random.rand(20, 20)
-    np.save(my_test_file, original_data)
+    reader = get_zarr_labels(out_pth)
+    assert callable(reader), "Get reader does not return reader given valid path"
 
-    # try to read it back in
-    reader = napari_get_reader(my_test_file)
-    assert callable(reader)
+    layer_data = reader(out_pth)
+    assert_array_almost_equal(data, layer_data[0][0]), "Reader did not return same data"
+    assert layer_data[0][2] == 'labels', "Reader did not return labels layer"
 
-    # make sure we're delivering the right format
-    layer_data_list = reader(my_test_file)
-    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-    layer_data_tuple = layer_data_list[0]
-    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+def test_get_label_image_stack_successful(tmp_path):
+    out_pth = os.path.join(tmp_path, 'out_stack.zarr')
+    label_data = np.random.randint(4, size=(3, 100, 100))
+    label_meta = {
+        'name': 'test_labels'
+    }
+    labels_layer = (label_data, label_meta, 'labels')
 
-    # make sure it's the same as it started
-    np.testing.assert_allclose(original_data, layer_data_tuple[0])
+    im_data = np.random.random((3, 100, 100))
+    im_meta = {
+        'name': 'test_im',
+        'rgb': False
+    }
+    im_layer = (im_data, im_meta, 'image')
+    ret_pth = write_label_image_pairs(out_pth, [labels_layer, im_layer])
 
+    reader = get_label_image_stack(ret_pth)
+    assert callable(reader), "Get reader does not return reader given valid image label stack"
+    
+    layers = reader(ret_pth)
+    layer_types = [layer[2] for layer in layers]
+    layer_names = [layer[1]['name'] for layer in layers]
+    layer_data = [layer[0] for layer in layers]
 
-def test_get_reader_pass():
-    reader = napari_get_reader("fake.file")
-    assert reader is None
+    assert sorted(layer_types) == ['image', 'labels'], "Reader does not load correct layer types"
+    assert sorted(layer_names) == ['test_im', 'test_labels'], "Reader does not preserve layer names"
+    assert layer_data[0].shape == im_data.shape, "Reader does not stack layers correctly"
--- napari_compressed_labels_io/_tests/test_writer.py
+++ napari_compressed_labels_io/_tests/test_writer.py
@@ -1,3 +1,38 @@
-# from napari_compressed_labels_io import napari_get_writer, napari_write_image
+from napari_compressed_labels_io import labels_to_zarr, label_image_pairs_to_zarr
+import os 
+import numpy as np
 
-# add your tests here...
+
+def test_labels_to_zarr_successful(tmp_path):
+    out_pth = os.path.join(tmp_path, 'out_labels.zarr')
+    data = np.random.randint(4, size=(3, 100, 100))
+    meta = {
+        'name': 'test_labels'
+    }
+    ret = labels_to_zarr(out_pth, data, meta)
+    assert isinstance(ret, str), "Saving valid labels to zarr does not return path"
+    assert os.path.exists(out_pth), "Saving valid data does not save a zarr to expected path"
+
+
+def test_label_image_pair_to_zarr_successful(tmp_path):
+    out_pth = os.path.join(tmp_path, 'out_stack.zarr')
+
+    ret = label_image_pairs_to_zarr(out_pth, ['image', 'labels'])
+    assert callable(ret), "Passing valid path and layer types does not return writer"
+    
+    label_data = np.random.randint(4, size=(3, 100, 100))
+    label_meta = {
+        'name': 'test_labels'
+    }
+    labels_layer = (label_data, label_meta, 'labels')
+
+    im_data = np.random.random((3, 100, 100))
+    im_meta = {
+        'name': 'test_im',
+        'rgb': False
+    }
+    im_layer = (im_data, im_meta, 'image')
+    ret_pth = ret(out_pth, [im_layer, labels_layer])
+    
+    assert isinstance(ret_pth, str), "Saving valid label image stack to zarr does not return path"
+    assert os.path.exists(out_pth), "Saving valid label image stack does not save a zarr to expected path"
